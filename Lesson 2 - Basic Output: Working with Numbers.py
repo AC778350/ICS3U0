e0ec252b0bc826267116652042d2c658c97d4af4
AC778350 <778350@pdsb.net>
2025-02-04 12:07:01 -0500

    Create Lesson 2 - Basic Output: Working with Numbers.py

---
+++ Lesson 2 - Basic Output: Working with Numbers.py
@@ -0,0 +1,16 @@
+# addition
+amount = 8
+amount += 1
+print("The amount is:" , amount)
+# multiplication
+amount = 8
+amount *= 2
+print(amount)
+# subtraction
+q = 18
+q -= 1
+print("The value is now: ", q)
+# division
+q = 18
+q /= 2 
+print("The value is now: ", q)
