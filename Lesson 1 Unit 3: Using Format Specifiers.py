d6b025e7413c6a9ebc2c75a0610fa3aec02e02ad
AC778350 <778350@pdsb.net>
2025-02-05 14:04:33 -0500

    Create Lesson 1 Unit 3: Using Format Specifiers.py

---
+++ Lesson 1 Unit 3: Using Format Specifiers.py
@@ -0,0 +1,15 @@
+print("There are ", 20, " people in this room.");
+# using a format specifier:
+print("There are %d people in this room." % 20)
+
+print("The total money spent is: ", (1.75+7.25+13.32))
+# using a format specifier:
+print("The total money spent is: %.2f." % (1.75+7.25+13.32))
+
+print("Pi is equal to %f." % 3.1416)
+
+print("%s's final exam score is %d." % ("Bob", 89))
+
+print("The area of a circle of radius %d is %.4f units squared." % (7, 3.1416*7*7))
+
+print("%s's earnings for the week is %.2f." % ("Annie", 111.50))
