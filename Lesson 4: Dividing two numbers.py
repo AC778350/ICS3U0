bbc3e63268c9b21f780740e1430ceab38b7cd553
AC778350 <778350@pdsb.net>
2025-02-10 12:17:44 -0500

    Create Lesson 4: Dividing two numbers.py

---
+++ Lesson 4: Dividing two numbers.py
@@ -0,0 +1,7 @@
+num = input("Please input a whole number: ") 
+num = int(num) 
+num2 = num2 = input("Use any whole number except zero:")
+num2 = int(num2)
+num3 = num / num2
+print("When", num, " is divided by", num2, end=" ")
+print("the result is:", num3)
