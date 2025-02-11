85af60caad4c8b83649ec099d37e167119cd1b38
AC778350 <778350@pdsb.net>
2025-02-11 13:54:06 -0500

    Lesson 7- Input and Output: Working Conditionals and Numbers.py

--- e.py
+++ e.py
@@ -1,15 +1,19 @@
-import math
-
-x = input("Please input a whole number: ")
+x = input("Please input a whole number between 1 and 20:")
 x = int(x)
-y = input("Please input another nonzero whole number")
-y = int(y)
-if y == 0:
-  print("Error, y cannot equal 0. A factor cannot be determined.")
-else:
-  print("Now deciding if", y, "is a factor of", x, "...")
-  rem = x % y
-  if rem == 0:
-    print("Yes!", y, "is a factor of", x)
+if (0 < x) and (x <= 20):
+  y = input("Please input another nonzero whole number between 1 and 20:")
+  y = int(y)
+  if (0 < y)and (y <= 20):
+    if y == 0:
+      print("Error, y cannot equal 0. A factor cannot be determined.")
+    else:
+      print("Now deciding if", y, "is a factor of", x, "...")
+      rem = x % y
+      if rem == 0:
+        print("Yes!", y, "is a factor of", x)
+      else:
+        print("No,", y, "is not a factor of", x)
   else:
-    print("No,", y, "is not a factor of", x)
+      print(y, "is outside of the range, cannot continue.")
+else:
+  print(x, "is outside of the range, cannot continue.")
