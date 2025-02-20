f03b988745b4e62b14bb513f85749c72e176c6c5
AC778350 <778350@pdsb.net>
2025-02-20 12:54:39 -0500

    Update Three-Part Output with Math.py

--- Three-Part Output with Math.py
+++ Three-Part Output with Math.py
@@ -18,7 +18,7 @@ if a >= 0:
     print("Error, you can only use positive numbers.")
 else:
   print("Error, you can only use positive numbers.")
-
+print(" ")
 print("I will find the cube's inner diagonal for any edge length!")
 m = input("Please enter the edge length of your cube:")
 m = float(m)
@@ -26,11 +26,15 @@ if m >= 0:
   print("The length of the inner diagonal of a cube with side length of", m, "is: %.2f" % (m*math.sqrt(3)))
 else:
   print("Error, the side length must be greater than 0.")
-
+print(" ")
 import math
 print("Welcome to the change calculator!")
 c = input("Please input your amount of change in cents:")
 c = float(c)
+q = 0
+d = 0
+n = 0
+p = 0
 c = c % 100
 if c >= 25:
   q = c/25
@@ -50,15 +54,42 @@ if c >= 25:
     else:
       if c >= 1:
         p = c/1
-        p = int(p)
+        p = int(p) 
   else:
     if c >= 5:
       n = c/5
       n = int(n)
       c = c % 5
+    else:
+      if c >= 1:
+        p = c/1
+        p = int(p)
 else:
   if c >= 10:
     d = c/10
     d = int(d)
     c = c % 10
-print("Your change under a dollar is", q, "quarters", d, "dimes", "nickels", p, "pennies.")
+    if c>= 5:
+      n = c/5
+      n = int(n)
+      c = c % 5
+      if c >= 1:
+        p = c/1
+        p = int(p)
+    else:
+      if c >= 1:
+        p = c/1
+        p = int(p)
+  else:
+    if c>= 5:
+      n = c/5
+      n = int(n)
+      c = c % 5
+      if c >= 1:
+        p = c/1
+        p = int(p)
+    else:
+      if c >= 1:
+        p = c/1
+        p = int(p)
+print("Your change under a dollar is", q, "quarters,", d, "dimes,", n, "nickels and", p, "pennies.")
