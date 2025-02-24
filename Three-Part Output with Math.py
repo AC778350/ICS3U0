1ee449f33f8d5793e5308b49ae33b3c357b294c3
AC778350 <778350@pdsb.net>
2025-02-24 12:13:22 -0500

    Update Three-Part Output with Math.py

--- Three-Part Output with Math.py
+++ Three-Part Output with Math.py
@@ -15,13 +15,13 @@ if a >= 0 or b >= 0: # compares the inputted numbers to make sure they are not e
 else: # an error message for if the user inputs any numbers lower than zero
   print("Error, you can only use positive whole numbers.") # lets the user know that they can only input positive numbers
 print(" ") # creates a space between the previous and next program
-print("I will find the cube's inner diagonal for any edge length!") # introduces and explains the purpose of the program to the user
-m = input("Please enter the edge length of your cube:") # has the user input the side length of the cube 
+print("I will find the cube's inner diagonal for any side length!") # introduces and explains the purpose of the program to the user
+m = input("Please enter the side length of your cube:") # has the user input the side length of the cube 
 m = float(m)
 if m == 0: # compares the input to zero, as a side length cannot be zero or less
-  print("The length of the inner diagonal of a cube with side length of", m, "is: %.2f" % (m*math.sqrt(3))) # solves the equation using the user's input and rounds to two decimal places
-else: # provides an error message if the input is <= 0
   print("Error, the side length must be greater than 0.") # lets the user know they have inputted an invalid number
+else: # provides an error message if the input is <= 0
+  print("The length of the inner diagonal of a cube with side length of", m, "is: %.2f" % (m*math.sqrt(3))) # solves the equation using the user's input and rounds to two decimal places
 print(" ") # creates a space between the previous and next program
 print("Welcome to the change calculator!") # introduces the user to the program
 c = input("Please input your amount of change in cents:") # prompts the user to input their amount of change 
@@ -43,27 +43,39 @@ if c >= 25: # compares c to 25 to see if any quarters can be used
       n = c/5 # the number of nickels equals the number of cents divided by five
       n = int(n) # turns n into an integer / whole number
       c = c % 5 # the remainder of 5 is the number of cents left after the nickels have been accounted for
-      if c >= 1:
-        p = c/1
-        p = int(p)
-    else:
-      if c >= 1:
+      if c >= 1: # compares the number of cents to 1 to see if any pennies can be used
+        p = c/1 # the number of pennies equals the number of cents divided by one
+        p = int(p) # turns p into an integer / whole number
+    else: # if there are no nickels, the code will check if there are pennies.
+      if c >= 1: # repeats the code for if there are pennies.
         p = c/1
         p = int(p) 
-  else:
-    if c >= 5:
+  else: # if there are no dimes, the code will check if there are nickels and/or pennies.
+    if c >= 5: # repeats the code for if there are nickels.
       n = c/5
       n = int(n)
       c = c % 5
-    else:
-      if c >= 1:
+    else: # repeats the code for if there are pennies.
+      if c >= 1: 
         p = c/1
         p = int(p)
-else:
-  if c >= 10:
+else: # if there are no quarters, the code will check if there are dimes, nickels and/or pennies.
+  if c >= 10: # repeats the code for if there are dimes.
     d = c/10
     d = int(d)
     c = c % 10
+    if c>= 5: # repeats the code for if there are nickels.
+      n = c/5
+      n = int(n)
+      c = c % 5
+      if c >= 1: # repeats the code for if there are pennies.
+        p = c/1
+        p = int(p)
+    else: # repeats the code for if there are no nickels.
+      if c >= 1:
+        p = c/1
+        p = int(p)
+  else: # repeats the code for if there are no dimes.
     if c>= 5:
       n = c/5
       n = int(n)
@@ -75,16 +87,4 @@ else:
       if c >= 1:
         p = c/1
         p = int(p)
-  else:
-    if c>= 5:
-      n = c/5
-      n = int(n)
-      c = c % 5
-      if c >= 1:
-        p = c/1
-        p = int(p)
-    else:
-      if c >= 1:
-        p = c/1
-        p = int(p)
-print("Your change under a dollar is", q, "quarters,", d, "dimes,", n, "nickels and", p, "pennies.") # lets the user know how many of each coin they need
+print("Your change under a dollar is", q, "quarters,", d, "dimes,", n, "nickels and", p, "pennies.") # lets the user know how many of each coin they have based on their change input
