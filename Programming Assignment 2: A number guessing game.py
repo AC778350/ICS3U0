45dc72b52dd999977e22fdf0f25f4718ac5635df
AC778350 <778350@pdsb.net>
2025-03-05 14:04:40 -0500

    Update Programming Assignment 2: A number guessing game.py

--- Programming Assignment 2: A number guessing game.py
+++ Programming Assignment 2: A number guessing game.py
@@ -7,7 +7,6 @@ num = random.randrange(1,100,1)
 guess = -1
 correct = False
 guessnumber = 1
-count = 6
 print ("Hello! Welcome to the number guessing game!")
 print ("I am thinking of a number between 1 and 100. It is your turn to guess what it is. You have a maximum of six (6) tries.")
 while not correct:
@@ -16,13 +15,14 @@ while not correct:
     correct = (guess == num)
     if correct:
         print("You guessed right! The number is:", num)
+        print("Thanks for playing!")
     else:
       guessnumber = guessnumber + 1
-      count = count - 1
       if num > guess:
         print("Higher!")
       if num < guess:
         print("Lower!")
-if count == 0:
-  print("Sorry, you are out of guesses! The answer was,", num)
-  print("Better luck next time!")
+      if guessnumber == 7:
+        print("Sorry, you are out of guesses! The answer was:", num)
+        print("Better luck next time!")
+        correct = True
