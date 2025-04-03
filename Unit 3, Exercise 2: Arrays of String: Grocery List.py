cebfc2285039c02968c14ce85ee6b3bb9ff1c2f5
AC778350 <778350@pdsb.net>
2025-04-03 13:40:52 -0400

    Update Unit 3, Exercise 2: Arrays of String: Grocery List.py

--- Unit 3, Exercise 2: Arrays of String: Grocery List.py
+++ Unit 3, Exercise 2: Arrays of String: Grocery List.py
@@ -1,9 +1,11 @@
-items = int(input("How many items are you entering?:"))
-num = items
+num = int(input("How many items are you entering?:"))
+items = num
 print("Enter your items now:")
 while items > 0:
   list = input("Item:")
   items -= 1
+
 print("You have entered", num, "items.")
+
 for c in range(len(list)):
-    print(c, list[c])
+  print(list[c])
