911671181b3bfd3a755a5663908f1379ec8e29ff
AC778350 <778350@pdsb.net>
2025-04-14 12:22:28 -0400

    Update Assignment 3: Is it a Palindrome? (Using loops and arrays).py

--- Assignment 3: Is it a Palindrome? (Using loops and arrays).py
+++ Assignment 3: Is it a Palindrome? (Using loops and arrays).py
@@ -1,5 +1,10 @@
-wordlist = ["school", "racecar", "pencil", "civic"]
-max = len(wordlist)
-for x in range(max):
-  print(wordlist[x])
-  wlen = len(wordlist[x])
+print("Palindrome Program!")
+print(" ")
+wordlist = ["racecar", "school", "civic", "pencil", "radar", "eraser", "kayak", "refer", "chair", "level"]
+for x in range(len(wordlist)):
+  word = wordlist[x]
+  for x in range(len(word) // 2):
+    if word[x] != word[len(word) - 1 - x]:
+      print(word, "is not a palindrome")
+    else:
+      print(word, "is a palindrome")
