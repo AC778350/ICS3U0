3ba8fb547f85368a014c27dd7bf3da500802b580
AC778350 <778350@pdsb.net>
2025-04-23 14:25:42 -0400

    Update Assignment 3: Is it a Palindrome? (Using loops and arrays).py

--- Assignment 3: Is it a Palindrome? (Using loops and arrays).py
+++ Assignment 3: Is it a Palindrome? (Using loops and arrays).py
@@ -1,41 +1,52 @@
-# Adena Chaudary
-# 778350
-# ICS3U0
+'''
+Adena Chaudary
+778350
+ICS3U0
 
-# VARIABLE DICTIONARY:
-# 1: wordlist - the list of words the program will
-# determine whether or not it is a palindrome
-# 2: x - a variable used to cycle through the list/words
-# 3: word - representative of each word in the list
+--- VARIABLE DICTIONARY: ---
+1: wordlist - the list of words used by the program
+2: word - representative of each word in the list
+3: palindrome - sets to True or False to determine if the word is a palindrome
+4: x - counting variable
+5: y - counting variable
+'''
 
 
+# introduces the program to the user
 print("Palindrome Program!")
-# introduces the program to the user 
 
+# the list of words that will be checked
 wordlist = ["racecar", "school", "civic", "pencil", "radar", "eraser", "kayak", "refer", "chair", "level"]
-# puts the words that will be checked for being palindromes in a list
 
+#
 for x in range(len(wordlist)):
-# "x" will cycle through the words in the list
-
-  word = wordlist[x]
+  
   #
-  
-  for x in range(len(word) // 2):
-  # "x" will cycle through each word in the list.
-  # uses integer division to get an even number of letters
-  # in each word so the letters can be compared
-  
-    if word[x] == word[len(word) - 1 - x]:
-    # 
-    
-      print(word, "is a palindrome")
-      # lets the user know the word is a palindrome
-      
-    else:
-    # if the 
-      print(word, "is not a palindrome")
-      # lets the user know the word is not a palindrome
-      
+  word = wordlist[x]
+
+  # sets the variable palindrome to True so it can be used
+  palindrome = True
+
+  #
+  for y in range(len(word) // 2):
+
+    #
+    if word[y] != word[len(word) - 1 - y]:
+
+      #
+      palindrome = False
+
+  #
+  if palindrome:
+
+    #
+    print(word, "is a palindrome")
+
+  #
+  else:
+
+    #
+    print(word, "is not a palindrome")
+
+#
 print("Goodbye!")
-# lets the user know the program is finished
