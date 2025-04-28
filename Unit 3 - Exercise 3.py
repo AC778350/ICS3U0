bc9cae4a1082574bef4eb9aa549c753757cd8e78
AC778350 <778350@pdsb.net>
2025-04-28 12:43:53 -0400

    Create Unit 3 - Exercise 3.py

---
+++ Unit 3 - Exercise 3.py
@@ -0,0 +1,12 @@
+items = ["Apples", "Bananas", "Cherries", "Dosa"]
+sizes = []
+for i in range(len(items)):
+    sizeI = len(items[i])
+    sizes.append(sizeI)
+
+for i in range(len(sizes)):
+  print("%d %s" % (sizes[i], items[i]))
+  if sizes[i] == len(items[i]):
+    print("True")
+  else:
+    print("False")
