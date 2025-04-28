04ad673d390d34c93bebad1be4de49127e638656
AC778350 <778350@pdsb.net>
2025-04-28 12:38:57 -0400

    Update Unit 3 - Exercise 2.py

--- Unit 3 - Exercise 2.py
+++ Unit 3 - Exercise 2.py
@@ -1,11 +1,9 @@
+grocerylist = []
 num = int(input("How many items are you entering?:"))
-items = num
-print("Enter your items now:")
-while items > 0:
-  list = input("Item:")
-  items -= 1
-
-print("You have entered", num, "items.")
-
-for c in range(len(list)):
-  print(list[c])
+print("Enter items now:")
+for i in range(num):
+  item = input("item:")
+  grocerylist.append(item)
+print("You have entered %d items." % len(grocerylist))
+for x in grocerylist:
+    print(x)
