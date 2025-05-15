f97dd66253a23ebea4c304b7a28c0a27adf7c0fe
AC778350 <778350@pdsb.net>
2025-05-15 14:24:53 -0400

    Update Assignment 4 - Reading Files and Searching for Data.py

--- Assignment 4 - Reading Files and Searching for Data.py
+++ Assignment 4 - Reading Files and Searching for Data.py
@@ -7,14 +7,24 @@ option = input("Enter w if you are looking for a word, or d for a word on a cert
 if option == "w":
     word = input("What word are you looking for?:")
 elif option == "d":
-    year = input("Enter the year:")
+    year = str(input("Enter the year:"))
     month = input("Enter the month (3-letter abbreviation, as in 'Jan' for 'January'):")
-    day = input("Enter the day:")
+    day = str(input("Enter the day:"))
 
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 for i in range(len(months)):
     if month == months[i]:
         month = i + 1
-        i += 1
     else:
         i += 1
+
+month = str(month)
+
+if len(day) == 1:
+    day = "0" + day
+if len(month) == 1:
+    month = "0" + month
+
+def date(year: str, month: str, day: str):
+    date = year+month+day
+    return int(date)
